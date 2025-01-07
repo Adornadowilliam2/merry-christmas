9cf19bc655c211749f5f17f6fe970d3bf000bc36
Adornadowilliam2 <adornadowilliam@gmail.com>
2025-01-08 07:29:20 +0800

    - Remove the christmas content - Change into ojt inputter lol

--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,17 +1,38 @@
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Container, ImageListItemBar, InputLabel, TextField, Typography } from '@mui/material'
 import React from 'react';
 import mockup from "../assets/bg-mockup.jpg"
 
 export default function Landingpage() {
   return (
       <Container className='flex-container'>
-          <Box className='image-container'>
-              <img src={mockup} alt="mockup-bg-image" />
+        <Container>
+        <Box sx={{display:"flex",justifyContent:'start',textAlign:"left", border:"1px solid black"}}>
+              <img src={"https://www.mfi.edu.ph/wp-content/uploads/2024/09/MFI-Logo_lr-1.png"} alt="mfi 50 anniversary logo"  style={{width:"100px"}}/> 
+            </Box>
+            <Typography sx={{textAlign:"center"}}>ON THE JOB TRAINING</Typography>
+          <Box sx={{display:"flex", justifyContent:"center"}}>
+          <Box sx={{display:"flex",alignItems:"center"}}>
+             <Box sx={{display:"flex",alignItems:"center"}}>
+            <InputLabel>Name:</InputLabel>
+            <input type="text" style={{borderTop:"none", borderLeft:"none", borderRight:"none", borderBottom:"1px solid black"}} />
+            </Box>
+             <Box sx={{display:"flex",alignItems:"center"}}>
+            <InputLabel>Name:</InputLabel>
+            <input type="text" style={{borderTop:"none", borderLeft:"none", borderRight:"none", borderBottom:"1px solid black"}} />
+            </Box>
           </Box>
-          <Box>
-              <Typography variant='h1'>Happy to be here </Typography>
-              <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, doloremque.</Typography>
+          <Box sx={{display:"flex",alignItems:"center"}}>
+             <Box sx={{display:"flex",alignItems:"center"}}>
+            <InputLabel>Name:</InputLabel>
+            <input type="text" style={{borderTop:"none", borderLeft:"none", borderRight:"none", borderBottom:"1px solid black"}} />
+            </Box>
+             <Box sx={{display:"flex",alignItems:"center"}}>
+            <InputLabel>Name:</InputLabel>
+            <input type="text" style={{borderTop:"none", borderLeft:"none", borderRight:"none", borderBottom:"1px solid black"}} />
+            </Box>
           </Box>
+          </Box>
+        </Container>
     </Container>
   )
 }
